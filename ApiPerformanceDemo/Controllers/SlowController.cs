e1028a1484d02f20206f80c2dde5750e3f0d7398
mostafa hseini <mstf.z.hoseini@gmail.com>
2026-02-17 12:52:41 +0330

    Add files via upload

---
+++ ApiPerformanceDemo/Controllers/SlowController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPerformanceDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SlowController : ControllerBase
+    {
+        [HttpGet("slow-data")]
+        public IActionResult GetSlowData()
+        {
+         
+            Thread.Sleep(3000); // 3 seconds blocking
+
+            var data = new List<string>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                data.Add("Item " + i);
+            }
+
+            return Ok(data);
+        }
+    }
+}
